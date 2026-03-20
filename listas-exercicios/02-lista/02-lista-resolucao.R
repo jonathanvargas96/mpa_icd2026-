64ce4487383eba5f00d3ef05096d182240112bca
Jonathan <jonathanvaargas@gmail.com>
2026-03-20 15:58:41 -0300

    update

--- listas-exercicios/02-lista/02-lista-resolucao.R
+++ listas-exercicios/02-lista/02-lista-resolucao.R
@@ -51,3 +51,69 @@ variaveis_sequencia <- gapminder |>
 
 variaveis_com_p <- gapminder |>
   select(contains("p") | ends_with("p"))
+
+# Exercício 7
+
+paises_america_2007 <- gapminder |>
+  filter(continent == "Americas" & year == "2007")
+
+# Exercício 8
+
+dados_brasil <- gapminder |> 
+  filter(country == "Brazil")
+
+# Exercício 9
+
+asia_pop_2007 <- gapminder |>
+  filter(continent == "Asia", pop > 50000000, year == "2007")
+  
+# Exercício 10
+
+paises_expVida_ppc <- gapminder |>
+  filter(lifeExp > 75, gdpPercap < 10000, year == "2007")
+
+# Exercício 11
+
+populacao_em_mi <- gapminder |>
+  mutate(pop_em_milhoes = pop / 1000000)
+
+# Exercício 12
+
+receita_total <- gapminder |>
+  mutate(gdp_total = gdpPercap * pop)
+
+# Exercício 13
+
+maiores_populacoes <- gapminder |> 
+  mutate(economia_grande = ifelse(pop > 50000000, "Sim", "Não"))
+
+# Exercício 14
+
+paises_expVida_ranking <- gapminder |> 
+  filter(year == 2007) |> 
+  mutate(lifeExp_ranking = case_when(
+      lifeExp < 60 ~ "Baixa",
+      lifeExp >= 60 & lifeExp <= 75 ~ "Média",
+      lifeExp > 75 ~ "Alta"))
+
+# Exercício 15
+
+expectativa_por_continente <- gapminder |>
+  group_by(continent) |>
+  summarise(expectativa_media = mean(lifeExp))
+
+# Exercício 16
+
+populacao_por_continente <- gapminder |>
+  group_by(continent) |>
+  summarise(populacao_total = sum(pop))
+
+# Exercício 17
+
+desempenho_filiais <- gapminder |> 
+  filter(year == 2007) |> 
+  group_by(continent) |> 
+  summarise(n_filiais = n(), pib_medio = mean(gdpPercap),
+            pib_maximo = max(gdpPercap))
+
+# Exercício 18
